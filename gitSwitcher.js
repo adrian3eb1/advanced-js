8a09e0aa1d393b5c9a09a4e9e94d218dfc802009
Adrian <adrian3eb1@me.com>
2016-05-09 12:56:49 -0700

    gitSwitcher homework

---
+++ gitSwitcher.js
@@ -0,0 +1,41 @@
+//Lesson 4 homework Switch exercise 5/9/2016
+
+
+var dow = 'Monday'
+
+switch (dow) {
+
+    case 'Monday':
+        console.log("Monday should be optional");
+        break;
+
+    case 'Tuesday':
+        console.log("JS class!!");
+        break;
+
+    case 'Wednesday':
+        console.log("Half way there");
+        break;
+
+    case 'Thursday':
+        console.log('JS class, round two');
+        break;
+
+    case 'Friday':
+        console.log('Smile its Friday');
+        break;
+
+    case 'Saturday':
+        console.log('Oh the possibilities');
+        break;
+
+    case 'Sunday':
+        console.log('Family, food, relaxing & GoT');
+        break;
+
+    default:
+        console.log('Sorry, the input you entered does not exist');
+        break
+
+
+}
